7b42a1015ce5de7d51125c64eb2bf3185e304f4f
臧俊通 <1406913232@qq.com>
2020-01-01 20:22:56 +0800

    组题列表结构搭建完成

--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -1,6 +1,56 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">组题列表</div>
+    
+   <el-table
+      :data="list"
+      border
+      style="width: 100%">
+      <el-table-column
+        :prop="id"
+        label="序号"
+        width="60px"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="addTime"
+        label="组题时间"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="userName"
+        label="用户名"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="questionIDs"
+        label="试题ID"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="progressOfAnswer"
+        label="答题进度"
+        >
+      </el-table-column>
+       <el-table-column
+        prop="accuracyRate"
+        label="正确率"
+        >
+      </el-table-column>
+       <el-table-column
+        prop="totalSeconds"
+        label="答题总耗时（秒）"
+        >
+      </el-table-column>
+       <el-table-column
+        prop="questionType"
+        label="组题类型/详情"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -8,7 +58,9 @@
 export default {
   name: 'QuestionsRandoms',
   data() {
-    return {}
+    return {
+      list:[]
+    }
   }
 }
 </script>
