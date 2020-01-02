50535990175bf274d8880258dc185bceecba7565
臧俊通 <1406913232@qq.com>
2020-01-02 11:57:56 +0800

    组题列表完成

--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -4,11 +4,12 @@
    <el-table
       :data="list"
       border
+      
       style="width: 100%">
       <el-table-column
-        prop="id"
+        prop="numberid"
         label="序号"
-        width="60px"
+        width="60"
         >
       </el-table-column>
       <el-table-column
@@ -19,6 +20,7 @@
       <el-table-column
         prop="userName"
         label="用户名"
+        width="80"
         >
       </el-table-column>
       <el-table-column
@@ -47,30 +49,73 @@
         >
       </el-table-column>
       <el-table-column
+      prop="id"
         label="操作">
-        <el-button size='small' type="text">删除</el-button>
+        <el-button @click="delQuestions({'id':510000197405202172})" size='small' type="text">删除</el-button>
       </el-table-column>
     </el-table>
+    <el-row type='flex' justify='center' align="middle" style="height:80px">
+      <span>共{{toatals}}条</span>
+      <el-pagination
+        :page-size='page.pageSize'
+        :current-page='page.currentPage'
+        background
+        layout="prev, pager, next"
+        @current-change='pagechange'
+        :total="page.total">
+      </el-pagination>
+    </el-row>
   </div>
 </template>
 
 <script>
-import {randoms} from '../../api/hmmm/questions'
-import { log } from 'util'
+import {randoms,removeRandoms} from '../../api/hmmm/questions'
+
 export default {
   name: 'QuestionsRandoms',
   data() {
     return {
+      // numberid:-1,
+      toatals:'',
       list:[],
-      result:{}
+      result:{},
+       page: {
+        total: 0,
+        pageSize: 10,
+        currentPage: 1
+      }
     }
   },
-  methods:{
+  methods:{ 
+    pagechange (newpage) {
+      this.page.currentPage = newpage
+      this.getListOfGroupQuestions()
+    },
+    delQuestions(id){
+      // console.log(id)
+      removeRandoms(id).then(res=>{
+      
+         this.getListOfGroupQuestions()
+         
+      })
+    },
     getListOfGroupQuestions(){
-      randoms().then(res=>{
-        console.log(res);
+      // console.log(11)
+      randoms({page:this.page.currentPage,pagesize:this.page.pageSize}).then(res=>{
+        // console.log(res);
+        this.page.total=res.data.pages
+        this.toatals=res.data.counts
+        this.list=res.data.items.map((item,index)=>{
+          // this.numberid=item.id
+          item.numberid=index+1
+          // item.id=index+1
+          item.progressOfAnswer=item.progressOfAnswer+'%'
+          item.accuracyRate= item.accuracyRate+'%'
+          item.questionIDs= item.questionIDs[0]
+         return item
+        })
+        // console.log(this.list);
         
-        this.list=res.data.items
       })
     }
   },
