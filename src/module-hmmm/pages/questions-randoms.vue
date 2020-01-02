67e0c61e0f29dfb0bde1791f96b3d87c3cd5a0c5
unknown <fanyoufu@outlook.com>
2020-01-02 14:37:24 +0800

    组题列表渲染成功

--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -1,16 +1,65 @@
 <template>
-  <div class="dashboard-container">
-    <div class="app-container">组题列表</div>
-  </div>
+  <el-table
+    :data="list"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="id"
+      label="序号"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="addTime"
+      label="组题时间"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="userName"
+      label="用户名">
+    </el-table-column>
+     <el-table-column
+      prop="questionIDs"
+      label="试题ID">
+    </el-table-column>
+     <el-table-column
+      prop=" progressOfAnswer"
+      label=答题进度>
+    </el-table-column>
+     <el-table-column
+      prop="accuracyRate"
+      label="正确率">
+    </el-table-column>
+     <el-table-column
+      prop="totalSeconds"
+      label="答题总耗时（秒）">
+    </el-table-column>
+     <el-table-column
+      prop="questionType"
+      label="操作">
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
-export default {
-  name: 'QuestionsRandoms',
-  data() {
-    return {}
-  }
-}
+import {randoms} from '../../api/hmmm/questions.js'
+ export default {
+      data() {
+        return {
+          list:[]//等一个个数据接收返回结果
+        }
+      },
+      methods:{
+         aA(){
+            randoms().then(res=>{
+              this.list=res.data.items
+                 console.log(res)
+            })
+         }
+      },
+      created(){    //钩子函数就是页面渲染时执行的
+         this.aA();
+      }
+    }
 </script>
 
 <style scoped>
