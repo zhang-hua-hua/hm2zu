2c3a15ba770a84f6791573c0b028ca72f7481301
progress-more <991414354@qq.com>
2020-01-02 09:39:56 +0800

    标签管理-标题与搜索区域布局

--- src/module-hmmm/pages/tags.vue
+++ src/module-hmmm/pages/tags.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">标签管理</div>
+    <el-card>
+      <div slot="header">
+        <el-button type='primary'>新增标签</el-button>
+        <el-button type='primary'>返回学科</el-button>
+      </div>
+      <el-form label-width='100px'>
+        <el-form-item label='标签名称:'>
+          <el-input style="width:220px;margin-right:10px;"></el-input>
+          <el-button>清除</el-button>
+          <el-button type='primary'>搜索</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
