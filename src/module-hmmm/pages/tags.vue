f85c8b3c67e2dc5712e578a4a1fb9143ff1eb3dd
progress-more <991414354@qq.com>
2020-01-03 10:48:56 +0800

    标签管理-新增，修改，禁用，删除，搜索 完成

--- src/module-hmmm/pages/tags.vue
+++ src/module-hmmm/pages/tags.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="dashboard-container">
     <el-card>
       <div slot="header">
-        <el-button type='primary'>新增标签</el-button>
+        <el-button @click='dialogVisible = true' type='primary'>新增标签</el-button>
         <el-button type='primary'>返回学科</el-button>
       </div>
       <el-form label-width='100px'>
@@ -21,9 +20,9 @@
         <el-table-column :formatter='formatterState' label='状态' prop='state'></el-table-column>
         <el-table-column label='操作'>
           <template slot-scope="scope">
-            <el-button type="text" size="small">修改</el-button>
+            <el-button @click='modify(scope.row.id)' type="text" size="small">修改</el-button>
             <el-button @click='change(scope.row)' type="text" size="small">{{scope.row.state === 1 ? '禁用' : '开启'}}</el-button>
-            <el-button type="text" size="small">删除</el-button>
+            <el-button @click='deltag(scope.row.id)' type="text" size="small">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -37,16 +36,41 @@
           @current-change='changePage'>
         </el-pagination>
       </el-row>
+      <el-dialog
+        title="新建标签"
+        :visible="dialogVisible"
+        :show-close='false'>
+        <el-form ref="myForm" :model='formData' :rules='rules' label-width='80px'>
+          <el-form-item prop='tagName' label='标签名称'>
+            <el-input v-model="formData.tagName"></el-input>
+          </el-form-item>
+          <el-form-item prop='subjectID' label='学科'>
+            <el-select v-model="formData.subjectID">
+              <el-option v-for="item in subjects" :key="item.value" :label='item.label' :value='item.value'></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+           <el-row slot="footer" type='flex' justify='end'>
+              <el-button @click="dialogCancel">取 消</el-button>
+              <el-button @click='submitForm' type="primary">确 定</el-button>
+           </el-row>
+        
+      </el-dialog>
     </el-card>
-  </div>
 </template>
 
 <script>
-import { list, removeState as changeState } from '../../api/hmmm/tags'
+import { list, removeState as changeState ,
+remove as removeTag,
+update as updateTag,
+add as addTag,
+detail as detailTag} from '../../api/hmmm/tags';
+import { simple as getSubjects } from '../../api/hmmm/subjects'
 export default {
   name: 'TagsList',
   data() {
     return {
+      dialogVisible: false,
       dataList: [], //设置变量接收返回标签数据
       pageObj: {
         total: 0,
@@ -55,10 +79,68 @@ export default {
       },
       searchForm: {
         tagName: ''
-      }
+      },
+      formData: {
+        subjectID: '',
+        tagName: ''
+      }, //对话框数据
+      rules: {
+        // 校验规则中加入trigger后 可使共同使用一个表单组件 切换时不会直接校验 只有输入内容离开后才会校验
+        subjectID: [{required: true, message: '学科id不能为空',trigger: 'blur'}],
+        tagName: [{required: true, message: '标签名称不能为空',trigger: 'blur'}]
+      },
+      subjects: [] //接收返回的学科数据
     }
   },
   methods: {
+    // 点击修改
+    async modify (id) {
+      this.dialogVisible = true
+      // 将该id的标签内容显示到 对话框表单中 即将获取数据给表单数据对象
+       let res = await detailTag({id});
+        this.formData = res.data
+    },
+    // 提交 新增/修改标签数据
+    async submitForm () {
+      // 先手动校验
+      await this.$refs.myForm.validate();
+        // 校验成功 判断是新增还是修改 发送请求 增加数据
+        // 由于新增时 直接填入数据 so只有表单数据
+        // 若是修改 需获取特定标签详情 并将返回数据赋给表单数据 
+        // 又根据接口 标签详情返回数据中有id so此时表单数据中也会有 而新增时没有
+        this.formData.id ? await updateTag(this.formData) :
+        await addTag(this.formData);
+          this.$message({
+            type:'success',
+            message: '操作成功'
+          });
+         // 添加成功后 清除内容 关闭对话框 重新获取数据
+        this.dialogCancel()
+        this.getDataList(this.searchForm)
+    },
+    // 对话框取消 清空数据
+    dialogCancel () {
+      this.formData = {
+        subjectID: '',
+        tagName: ''
+      };
+      this.dialogVisible = false //关闭弹层
+    },
+    // 删除标签
+     async deltag (id) {
+      // 确认删除
+       try {
+        await this.$confirm('您是否确定要删除此数据？');
+         await removeTag ({id});
+      this.$message({ type: 'success', message: '删除成功' });
+      this.getDataList(this.searchForm) //重新带条件查询数据
+      } catch (error) {
+        this.$message({
+            type: 'info',
+            message: '已取消删除'
+      })
+      }
+    },
     // 关闭或开启禁用
     async change (row) {
       await this.$confirm('确定改变状态吗?',{
@@ -107,10 +189,17 @@ export default {
         this.dataList = res.data.items
         this.pageObj.total = res.data.counts
       })
+    },
+    // 获得简单学科列表
+    getSubjects () {
+      getSubjects().then(res => {
+        this.subjects = res.data
+      })
     }
   },
   created () {
     this.getDataList()
+    this.getSubjects()
   }
 }
 </script>
